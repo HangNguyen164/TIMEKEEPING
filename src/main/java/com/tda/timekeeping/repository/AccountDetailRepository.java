459ff4a6393c98e64a53a09f262a8e53a4a612bc
HangNT164 <hangnt16499@gmail.com>
2021-01-06 22:38:05 +0700

    cm

--- src/main/java/com/tda/timekeeping/repository/AccountDetailRepository.java
+++ src/main/java/com/tda/timekeeping/repository/AccountDetailRepository.java
@@ -31,6 +31,6 @@ public interface AccountDetailRepository extends JpaRepository<AccountDetail, In
     @Query(value = "UPDATE  AccountDetail  a SET a.startTime=:start_time, a.endTime=:end_time, a.note=:note, a.checkEmail=:send_mail WHERE a.id=:id")
     void update(@Param("start_time") Time startTime, @Param("end_time") Time endTime, @Param("note") String note, @Param("send_mail") int sendMail, @Param("id") int id);
 
-    @Query(value = query+" WHERE  ad.username=:username AND ad.workDate=:work_date")
+    @Query(value = "SELECT * FROM account_detail ad WHERE ad.username=:username AND ad.work_date=:work_date", nativeQuery = true)
     AccountDetail getOneByUsernameAndDate(@Param("username")String username, @Param("work_date")Date workDate);
 }
