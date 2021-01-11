7ae2d9ab68c832ec84b80e8c91a3d7a4a510291b
DELL <hangnt1649@gmail.com>
2021-01-11 18:51:13 +0700

    Login

--- src/main/java/com/tda/timekeeping/security/WebSecurityConfig.java
+++ src/main/java/com/tda/timekeeping/security/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/home-admin/**")
+                .failureUrl("/login?error")
                 .permitAll(false)
                 .and()
                 .logout().permitAll();
